0368ff415b3b1d3ee11fdd716ea700a65295321e
Marlon Derlam <suporte03@sollucao.net>
2026-07-12 18:28:32 -0300

    Ajusta base para EvoluaFit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: process.env.NODE_ENV === 'production' ? '/Mundo-do-Esporte/' : '/',
-=======
-  base: '/EvoluaFit/',
->>>>>>> c8796498efbab4642c148fd6ac0020e8ff2b5889
+  base: process.env.NODE_ENV === 'production' ? '/EvoluaFit/' : '/',
 })
